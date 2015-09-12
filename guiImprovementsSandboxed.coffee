de15942c71abde6e174437a6ad4ac1c182f702bf
ahto <weirdusername@gmail.com>
2015-09-11 20:24:58 -0500

    Messages: select all properly highlights

--- guiImprovementsSandboxed.coffee
+++ guiImprovementsSandboxed.coffee
@@ -4,7 +4,7 @@
 // @name         FlightRising GUI Improvements Sandboxed Portion
 // @description  Runs potentially unsafe code in a protective sandbox.
 // @namespace    ahto
-// @version      1.1.0
+// @version      1.1.1
 // @include      http://*flightrising.com/*
 // @require      https://greasyfork.org/scripts/10922-ahto-library/code/Ahto%20Library.js?version=61626
 // @grant        GM_addStyle
@@ -23,11 +23,17 @@ if new RegExp('http://www1\.flightrising\.com/msgs$', 'i').test document.locatio
         }
     ''')
 
-    findMatches('#ajaxbody tr input[type=checkbox]').click (event) ->
-        target = $(event.target)
-        tr = target.parents('tr')
+    updateCheckbox = (targetCheckbox) ->
+        tr = targetCheckbox.parents 'tr'
 
-        if target.prop('checked')
-            tr.addClass('selected-tr')
+        if targetCheckbox.prop 'checked'
+            tr.addClass 'selected-tr'
         else
-            tr.removeClass('selected-tr')
+            tr.removeClass 'selected-tr'
+
+    findMatches('#ajaxbody tr input[type=checkbox]').click (event) ->
+        updateCheckbox $ event.target
+
+    findMatches('input#set', 1, 1).click (event) ->
+        for i in findMatches('#ajaxbody tr input[type=checkbox]')
+            updateCheckbox $ i
--- guiImprovementsSandboxed.js
+++ guiImprovementsSandboxed.js
@@ -5,22 +5,36 @@
 // @name         FlightRising GUI Improvements Sandboxed Portion
 // @description  Runs potentially unsafe code in a protective sandbox.
 // @namespace    ahto
-// @version      1.1.0
+// @version      1.1.1
 // @include      http://*flightrising.com/*
 // @require      https://greasyfork.org/scripts/10922-ahto-library/code/Ahto%20Library.js?version=61626
 // @grant        GM_addStyle
 // ==/UserScript==
  */
+var updateCheckbox;
+
 if (new RegExp('http://www1\.flightrising\.com/msgs$', 'i').test(document.location.href)) {
   GM_addStyle('#ajaxbody tr.highlight-tr.selected-tr {\n    background-color: #CAA;\n}\n\n#ajaxbody tr.selected-tr {\n    background-color: #CBB;\n}');
-  findMatches('#ajaxbody tr input[type=checkbox]').click(function(event) {
-    var target, tr;
-    target = $(event.target);
-    tr = target.parents('tr');
-    if (target.prop('checked')) {
+  updateCheckbox = function(targetCheckbox) {
+    var tr;
+    tr = targetCheckbox.parents('tr');
+    if (targetCheckbox.prop('checked')) {
       return tr.addClass('selected-tr');
     } else {
       return tr.removeClass('selected-tr');
     }
+  };
+  findMatches('#ajaxbody tr input[type=checkbox]').click(function(event) {
+    return updateCheckbox($(event.target));
+  });
+  findMatches('input#set', 1, 1).click(function(event) {
+    var i, j, len, ref, results;
+    ref = findMatches('#ajaxbody tr input[type=checkbox]');
+    results = [];
+    for (j = 0, len = ref.length; j < len; j++) {
+      i = ref[j];
+      results.push(updateCheckbox($(i)));
+    }
+    return results;
   });
 }
